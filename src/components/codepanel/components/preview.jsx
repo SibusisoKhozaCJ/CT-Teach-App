16222fc7c4a05c9ee383f554eb19bcf9b81c3983
Roman Kovalenko <kovalenkoor@gmail.com>
2021-02-22 22:31:03 +0600

    update: codepanel prevew add placeholder text when nothing to preview

--- src/components/codepanel/components/preview.jsx
+++ src/components/codepanel/components/preview.jsx
@@ -25,6 +25,19 @@ const useStyles = makeStyles(() => ({
     width: "100%",
     height: "100%",
     border: "none"
+  },
+  emptyPreview: {
+    padding: 0,
+    paddingTop: 50,
+    width: "100%",
+    height: "100%",
+    border: "none",
+    fontSize: 16,
+    color: "#bbb",
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: "auto",
   }
 }));
 
@@ -34,6 +47,14 @@ const Preview = () => {
 
   return (
     <div className={classes.previewContainer}>
+      {preview.length > 0 ? (
+        <iframe title="code preview" className={classes.previewIframe} srcDoc={preview} />
+      ) : (
+        <div className={classes.emptyPreview}>
+            <p>No previw available</p>
+            <p>time to get coding</p>
+        </div>
+      )}
       <iframe title="code preview" className={classes.previewIframe} srcDoc={preview} />
       <div className={classes.previewOverlay} />
     </div>
