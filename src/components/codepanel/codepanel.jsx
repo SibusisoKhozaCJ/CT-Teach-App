47579bd5436f27987db8d2d02d6230f3127bd6d7
Faizan Latif <faizanla122@gmail.com>
2021-04-15 14:18:58 +0500

    fix:Firefox Desktop: Certain Slides: Which clicked on upper right quarter, the whole page/app goes blank.

--- src/components/codepanel/codepanel.jsx
+++ src/components/codepanel/codepanel.jsx
@@ -65,6 +65,7 @@ const Codepanel = ({
   };
 
   const slideChangeHandler = (e) => {
+    e.preventDefault();
     if (e.target.value === 'top') {
       dispatch(codepanelSetSlideNumber(currentSlideNumber - 1));
     }
