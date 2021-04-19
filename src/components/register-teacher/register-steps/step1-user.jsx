200a201c0f37489a380bd761757f7eb7001c6b8a
Heena trivedi <trivediheenalove@gmail.com>
2021-04-19 13:08:17 +0530

    DoB Day, Month & Year selection should be in standard, mobile-friendly, mac-friendly drop-down.

--- src/components/register-teacher/register-steps/step1-user.jsx
+++ src/components/register-teacher/register-steps/step1-user.jsx
@@ -1,12 +1,37 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
+import AutocompleteSelect from "../../../shared/components/inputs/AutocompleteSelect";
 
-const UserRegisterStep1 = ({ onUpdate, form }) => {
+const daysOfTheMonth = Array(31)
+    .fill()
+    .map((_, idx) => (1 + idx).toString())
+    .map(option => option.length === 1 ? `0${option}` : option);
+const UserRegisterStep1 = ({ onUpdate, form, formOptions }) => {
+  const { dob} = form;
+  const [month, day, year] = dob ? dob.split('/') : ['', '', ''];
   const handleFormEdit = (key) => (event) =>
     onUpdate({ ...form, [key]: event.target.value });
-
+    const handleDobEdit = key => val => {
+      let newDob;
+      if (key === 'year') {
+          newDob = [month, day, val];
+      } else if (key === 'month') {
+          newDob = [val, day, year];
+      } else if (key === 'day') {
+          newDob = [month, val, year];
+      } else return;
+      onUpdate({...form, 'dob': newDob.join('/')})
+    
+  };
+  const yearOptions = useMemo(() => {
+    return (
+        Array(112)
+        .fill(10)
+        .map((value, idx) => (2021-idx).toString())
+    );
+}, [])
   return (
     <>
       <div className="registration-form">
@@ -41,35 +66,59 @@ const UserRegisterStep1 = ({ onUpdate, form }) => {
           <Grid container spacing={3}>
             <Grid item xs={4}>
               <Box my={1}>
-                <TextField
+                {/* <TextField
                   fullWidth
                   label="Day"
                   variant="outlined"
                   value={form.day}
                   onChange={handleFormEdit("day")}
-                />
+                /> */}
+                <AutocompleteSelect
+                  type="number"
+                  maxDigits={2}
+                  label="Day"
+                  selectedValue={day}
+                  options={daysOfTheMonth}
+                  onSelect={handleDobEdit('day')}
+                  />
               </Box>
             </Grid>
             <Grid item xs={4}>
               <Box my={1}>
-                <TextField
+                {/* <TextField
                   fullWidth
                   label="Month"
                   variant="outlined"
                   value={form.month}
                   onChange={handleFormEdit("month")}
-                />
+                /> */}
+                <AutocompleteSelect
+                  label="Month"
+                  type="number"
+                  maxDigits={2}
+                  selectedValue={month}
+                  options={['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']}
+                  onSelect={handleDobEdit('month')}
+                            />
               </Box>
             </Grid>
             <Grid item xs={4}>
               <Box my={1}>
-                <TextField
+                {/* <TextField
                   fullWidth
                   required
                   label="Year"
                   variant="outlined"
                   value={form.year}
                   onChange={handleFormEdit("year")}
+                /> */}
+                  <AutocompleteSelect
+                    label="Year"
+                    type="number"
+                    maxDigits={4}
+                    selectedValue={year}
+                    options={yearOptions}
+                    onSelect={handleDobEdit('year')}
                 />
               </Box>
             </Grid>
