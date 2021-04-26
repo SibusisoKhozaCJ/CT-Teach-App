d46a47ab56502d413a627845cb5b26dcc3997564
Heena trivedi <trivediheenalove@gmail.com>
2021-04-26 18:47:11 +0530

    Revise Desktop Main Menu

--- src/redux/reducers/sidebar-reducer.js
+++ src/redux/reducers/sidebar-reducer.js
@@ -8,6 +8,8 @@ export default function sideBarReducer(state = initialState, action) {
       return { ...state, isSidebarOpened: !state.isSidebarOpened };
     case Types.CLOSE_SIDEBAR:
       return { ...state, isSidebarOpened: false};
+      case Types.OPEN_SIDEBAR:
+      return { ...state, isSidebarOpened: true};
     default:
       return state;
   }
