05e459fcbcf370890f6165a5829bafbbc20d3245
Faizan Latif <faizanla122@gmail.com>
2021-04-29 07:28:27 +0500

    fixed:Safari (Desktop) the whole page goes blank after logging in. Serious glitch/bug. Can you solve this please. Thank you.

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Icon1 from "../../assets/icons/footer-icon/icon1.svg";
 import Icon3 from "../../assets/icons/footer-icon/icon3.svg";
 import Icon2 from "../../assets/icons/footer-icon/icon2.svg";
 import Icon4 from "../../assets/icons/footer-icon/arrw.svg";
 import Icon5 from "../../assets/icons/footer-icon/gallery.svg";
 import ProjectsIcon from "../../assets/images/rocket-icon.png";
-import { useHistory, useLocation } from "react-router-dom";
+import {useHistory, useLocation} from "react-router-dom";
 import routes from "../../routes";
-import { subscribe } from 'on-screen-keyboard-detector';
+import {subscribe} from 'on-screen-keyboard-detector';
 import {ProjectsSvg} from "../../shared/svgs/menu-items";
 
 const Footer = () => {
@@ -16,7 +16,7 @@ const Footer = () => {
     const [isLayoutRender, setIsLayoutRender] = useState(false);
     var splitPath = window.location.pathname.split('/')
     var path = splitPath[splitPath.length - 1];
-    const [keyboardExist, setKeyboardExist] = useState(false);
+    const [keyboardExist, setKeyboardExist] = useState(true);
 
     const shouldLayoutRender = (pathname) => {
         if (
@@ -29,39 +29,40 @@ const Footer = () => {
         return true;
     }
     useEffect(() => {
-        const unsubscribe = subscribe(visibility => {
-            if (visibility === "hidden"  ){
+
+        const initialHeight = window.innerHeight
+        window.addEventListener('resize', function (e) {
+            if (window.innerHeight != initialHeight && path === 'home') {
+                setKeyboardExist(keyboardExist => false)
+            } else {
                 setKeyboardExist(keyboardExist => true)
-            } else if(path === 'home' && visibility === "visible"){
-                setKeyboardExist(keyboardExist => false)
             }
-            else {
-                setKeyboardExist(keyboardExist => false)
-            }
-        });
+        })
+
         setIsLayoutRender(shouldLayoutRender(location.pathname));
     }, [location]);
+
     return (
-
-        ( isLayoutRender &&  keyboardExist && <footer className='footer-mobile'>
+        (isLayoutRender && keyboardExist && <footer className='footer-mobile'>
             <div className="footer-icon select-footer">
-                <img onClick={(evt)=>history.push("/tribe")} src={Icon3} className="coverage" alt="" />
+                <img onClick={(evt) => history.push("/tribe")} src={Icon3} className="coverage" alt=""/>
             </div>
 
             <div className="footer-icon">
-                <img onClick={(evt)=>history.push("/codepanel/C001/P001/T001")} src={ProjectsIcon} alt="" />
+                <img onClick={(evt) => history.push("/codepanel/C001/P001/T001")} src={ProjectsIcon} alt=""/>
             </div>
             <div className="footer-icon">
-                <img onClick={(evt)=>history.push("/tribe")} src={Icon5} className="coverage" alt="" />
+                <img onClick={(evt) => history.push("/tribe")} src={Icon5} className="coverage" alt=""/>
             </div>
             <div className="footer-icon centr-Ficon">
                 <div className="mt-ft-icon">
-                    <img onClick={(evt)=>history.push("/codepanel/5-min-website")} src={Icon4} className="coverage" alt="" />
+                    <img onClick={(evt) => history.push("/codepanel/5-min-website")} src={Icon4} className="coverage"
+                         alt=""/>
                 </div>
             </div>
 
             <div className="footer-icon">
-                <img onClick={(evt)=>history.push("/tribe")} src={Icon2} className="coverage" alt="" />
+                <img onClick={(evt) => history.push("/tribe")} src={Icon2} className="coverage" alt=""/>
             </div>
 
 
